6ffb4da6a2cb84bc12df37426555597edf2e919a
JexLes856 <jexles856@outlook.com>
2026-03-11 21:12:57 -0400

    soon to be final changes on looks

--- script.js
+++
@@ -1,9 +0,0 @@
-const select = document.getElementById('key');
-
-for (let i = 0; i <= 999; i++) {
-    const opt = document.createElement('option');
-    const val = i.toString().padStart(3, '0');
-    opt.value = val;
-    opt.textContent = val;
-    select.appendChild(opt);
-}
